8fdd285526ba4c462d1ae6389aacb4a12720bdb1
DanielaMontero08 <jeondanielamy@gmail.com>
2023-07-24 16:19:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function formatDate(timestamp){
+function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10){
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes < 10){
+  if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   let days = [
@@ -15,13 +15,13 @@ function formatDate(timestamp){
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayTemperature(response){
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
